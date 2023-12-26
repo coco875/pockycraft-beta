4a52c9039cbf33447c5fac9a3205e9d0296dbecf
coco875 <59367621+coco875@users.noreply.github.com>
2023-12-26 19:16:05 +0100

    update server.sh

--- server.sh
+++ server.sh
@@ -1,3 +1,5 @@
-rm mods/LegendaryTooltips*
-rm mods/embeddium*
-rm mods/darkness*
+cd mods
+rm LegendaryTooltips*
+rm embeddium*
+rm darkness*
+wget https://mediafilez.forgecdn.net/files/4979/21/Dynmap-3.7-beta-4-forge-1.19.2.jar
